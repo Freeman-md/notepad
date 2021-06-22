a04bb75077669b09c916b0427026b2ff5e3e1946
Freeman-md <freemansparda@gmail.com>
2021-06-23 00:02:52 +0100

    Initial Commit

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
 Vue.config.productionTip = false
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAMz4Uh6UDn1bi5G87P-Nls8kj17VBR8ks",
+    authDomain: "notepad-192e7.firebaseapp.com",
+    projectId: "notepad-192e7",
+    storageBucket: "notepad-192e7.appspot.com",
+    messagingSenderId: "733111826588",
+    appId: "1:733111826588:web:164951ba830b36031e9559",
+    measurementId: "G-T4PSGX618Y"
+})
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
